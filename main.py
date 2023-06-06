eb2e8cb3ea93a9d77fa0ea4c12d0d4509c535af7
Gorjan Spirovski <gorjanspirovski@gmail.com>
2023-06-06 10:58:21 +0200

    -Rabota so 2 ports (bez razlika koj kogaj e aktiven). -Kogaj ima nova kartica, se zapishue samo IDto i deneshniot (from) datum, a drugite polinja sa prazni.

--- main.py
+++ main.py
@@ -20,11 +20,24 @@ import datetime
 # Construct the file path relative to the base directory
 file_path = "/Users/gorjanspirovski/Desktop/Ivica app/pythonProject/data/card_ids.txt"
 
-port = '/dev/cu.usbserial-110'  # Replace with the correct serial port
 baud_rate = 9600
 
-# Open the serial port
-ser = serial.Serial(port, baud_rate)
+# Define the list of serial ports to try
+serial_ports = ['/dev/cu.usbserial-110', '/dev/cu.usbserial-10']
+
+# Try to open the serial port
+ser = None
+for port in serial_ports:
+    try:
+        ser = serial.Serial(port, baud_rate)
+        break
+    except serial.SerialException:
+        pass
+
+# Check if the serial port was successfully opened
+if ser is None:
+    print("Failed to open the serial port. Exiting...")
+    exit()
 
 # Open the text file in read mode
 file = open(file_path, 'r')
@@ -58,10 +71,10 @@ try:
             else:
                 # Card ID does not exist, add it to the file
                 from_date = datetime.date.today().strftime("%d-%m-%Y")
-                to_date = (datetime.date.today() + datetime.timedelta(days=30)).strftime("%d-%m-%Y")
-                existing_card_data.append([data, '', from_date, to_date])
+                #to_date = (datetime.date.today() + datetime.timedelta(days=30)).strftime("%d-%m-%Y")
+                existing_card_data.append([data, 'ime_prezime', from_date, 'to_date'])
                 file = open(file_path, 'a')
-                file.write(', '.join([data, '', from_date, to_date]) + '\n')
+                file.write(', '.join([data, 'ime_prezime', from_date, 'to_date']) + '\n')
                 file.close()
                 response = '0'  # Card ID does not exist
 
